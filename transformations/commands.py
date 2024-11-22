ede591425f54ad2b6ea76a67732e27383c01d1e0
Anton Kosovskii <anton@kosovski.com>
2024-11-22 12:38:38 +0200

    refactoring

--- transformations/commands.py
+++ transformations/commands.py
@@ -1,15 +1,19 @@
 import pandas as pd
 from typing import List
 
+
 def filter_data(df: pd.DataFrame, predicate: str) -> pd.DataFrame:
     return df.query(predicate)
+
 
 def select_columns(df: pd.DataFrame, columns: List[str]) -> pd.DataFrame:
     return df[columns]
 
+
 def add_column(df: pd.DataFrame, column_name: str, formula: str) -> pd.DataFrame:
     df[column_name] = df.eval(formula)
     return df
+
 
 # Mapping of command names to functions
 COMMANDS = {
